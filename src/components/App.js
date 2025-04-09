6f8e2ce0de0f821909486b4bed4c44038c67e43d
Pedro A. Góngora <pedro.gongora@gmail.com>
2025-04-08 20:26:32 -0600

    feat: filename in window title

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
+import { Helmet } from 'react-helmet'
 import ArcaDeNoesis from './ArcaDeNoesis'
+import { useSelector } from 'react-redux'
 
 function App() {
-  return <ArcaDeNoesis />
+  const filename = useSelector(state => state.dialectics.filename)
+  const windowTitle = `Arca de Noesis - ${filename ? filename : 'untitled'}.json`
+  return (
+    <>
+      <Helmet>
+        <title>{windowTitle}</title>
+      </Helmet>
+      <ArcaDeNoesis />
+    </>
+  )
 }
 
 export default App
